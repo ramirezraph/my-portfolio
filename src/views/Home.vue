cc3dfb7eece9c09698625d5014a7416846f4b388
Raphael Ramirez <ramirez.raph@yahoo.com>
2021-06-07 22:50:37 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="w-full h-full grid place-items-center">
+      <div>
+         <h1 class="text-6xl">Hello, World!</h1>
+         <p>This is Vue with TailwindCSS.</p>
+      </div>
+   </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+   name: 'Home',
+   components: {},
+};
+</script>

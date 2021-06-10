4026ac7d7969e5b535e87ac67bfbf0240d6b4740
Raph Ramirez <ramirez.raph@yahoo.com>
2021-06-10 21:37:48 +0800

    added responsive nav

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,9 +1,9 @@
 <template>
    <div>
       <nav
-         class="absolute left-0 right-0 z-50 hidden w-full p-6 space-x-6 lg:px-32 top-8"
+         class="absolute left-0 right-0 z-10 w-full px-6 space-x-6 lg:p-6 lg:px-32 top-8"
       >
-         <ul class="flex justify-end" id="nav">
+         <ul class="justify-end hidden lg:flex" id="nav">
             <li data-menuanchor="project">
                <a href="#project" class="px-3 text-white active:text-primary">
                   my projects
@@ -20,7 +20,114 @@
                </a>
             </li>
          </ul>
+
+         <!-- mobile nav -->
+         <div class="flex items-center lg:hidden">
+            <button @click="toggleNav">
+               <svg
+                  id="MDI_menu"
+                  data-name="MDI / menu"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+               >
+                  <g
+                     id="Boundary"
+                     fill="#fff"
+                     stroke="rgba(0,0,0,0)"
+                     stroke-width="1"
+                     opacity="0"
+                  >
+                     <rect width="32" height="32" stroke="none" />
+                     <rect x="0.5" y="0.5" width="31" height="31" fill="none" />
+                  </g>
+                  <path
+                     id="Path_menu"
+                     data-name="Path / menu"
+                     d="M3,6H26.919V8.694H3V6m0,6.734H26.919v2.694H3V12.734m0,6.734H26.919v2.694H3Z"
+                     transform="translate(1.04 1.919)"
+                     fill="#fff"
+                  />
+               </svg>
+            </button>
+         </div>
       </nav>
+      <!-- mobile full navigation -->
+      <transition-group name="slide" mode="out-in">
+         <div
+            class="absolute z-20 grid w-screen h-screen lg:hidden bg-primary opacity-95 place-items-center"
+            v-show="showMobileNav"
+            key="nav"
+         >
+            <div class="flex flex-col justify-center w-4/5 gap-y-6">
+               <span class="menu-text opacity-80">menu</span>
+               <ul class="space-y-8" id="nav">
+                  <li data-menuanchor="project">
+                     <a
+                        href="#project"
+                        class="px-3 text-3xl text-white active:text-primary"
+                        @click="toggleNav"
+                     >
+                        my projects
+                     </a>
+                  </li>
+                  <li data-menuanchor="about">
+                     <a
+                        href="#about"
+                        class="px-3 text-3xl text-white active:text-primary"
+                        @click="toggleNav"
+                     >
+                        about me
+                     </a>
+                  </li>
+                  <li data-menuanchor="contact">
+                     <a
+                        href="#contact"
+                        class="px-3 text-3xl text-white active:text-primary"
+                        @click="toggleNav"
+                     >
+                        get in touch
+                     </a>
+                  </li>
+               </ul>
+               <button class="p-8 mx-auto my-12" @click="toggleNav">
+                  <svg
+                     id="MDI_window-close"
+                     data-name="MDI / window-close"
+                     xmlns="http://www.w3.org/2000/svg"
+                     width="31.221"
+                     height="31.221"
+                     viewBox="0 0 31.221 31.221"
+                  >
+                     <g
+                        id="Boundary"
+                        fill="#fff"
+                        stroke="rgba(0,0,0,0)"
+                        stroke-width="1"
+                        opacity="0"
+                     >
+                        <rect width="31.221" height="31.221" stroke="none" />
+                        <rect
+                           x="0.5"
+                           y="0.5"
+                           width="30.221"
+                           height="30.221"
+                           fill="none"
+                        />
+                     </g>
+                     <path
+                        id="Path_window-close"
+                        data-name="Path / window-close"
+                        d="M16.005,14.106l7.207,7.207v1.9h-1.9l-7.207-7.207L6.9,23.212H5v-1.9l7.207-7.207L5,6.9V5H6.9l7.207,7.207L21.313,5h1.9V6.9Z"
+                        transform="translate(1.504 1.504)"
+                        fill="#fff"
+                     />
+                  </svg>
+               </button>
+            </div>
+         </div>
+      </transition-group>
       <full-page :options="options" id="fullpage" class="px-0 lg:px-32">
          <hero data-menuanchor="hero" v-if="true" class="section active" />
          <project-one data-menuanchor="project" v-if="true" class="section" />
@@ -73,6 +180,8 @@ export default {
    },
    data() {
       return {
+         showMobileNav: false,
+
          options: {
             licenseKey: 'AsdasSDasd',
             autoScrolling: true,
@@ -90,23 +199,26 @@ export default {
          },
       };
    },
+   methods: {
+      toggleNav() {
+         this.showMobileNav = !this.showMobileNav;
+      },
+   },
 };
 </script>
 
 <style lang="scss">
-// .name {
-//    position: relative;
+.slide-enter-active,
+.slide-leave-active {
+   transition: opacity 300ms ease-out, transform 300ms ease-out;
+}
 
-//    &:before {
-//       content: '';
-//       width: 20ch;
-//       height: 3px;
-//       background: #18f890;
-//       position: absolute;
-//       left: 0;
-//       bottom: -0.5em;
-//    }
-// }
+.slide-enter,
+.slide-leave-to {
+   opacity: 0;
+   transform: translateY(-50px);
+   transition: opacity 300ms ease-in, transform 300ms ease-in;
+}
 
 li a {
    transition: all 500ms ease-in-out;

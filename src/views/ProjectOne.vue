c63a3f8b584dc7d727d7fbd04a156cc780e4239b
Raph Ramirez <ramirez.raph@yahoo.com>
2021-06-12 22:09:21 +0800

    spacing between project title and project view

--- src/views/ProjectOne.vue
+++ src/views/ProjectOne.vue
@@ -1,6 +1,6 @@
 <template>
    <section class="text-left">
-      <h1 class="mt-3 text-4xl md:text-7xl lg:text-8xl">
+      <h1 class="mt-3 text-4xl md:text-7xl lg:text-8xl 3xl:mb-32">
          <span class="text-primary">$ cd</span> my project
       </h1>
       <project-view :project="projects[0]" class="mt-6 md:mt-16" />
